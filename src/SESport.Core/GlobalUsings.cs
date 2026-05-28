ebd6899bb7243e8691a4ea52ab7f02d888387fa9
Daniel Solin <daniel@solin.org>
2026-05-28 16:59:30 +0200

    Revmoed unused usings.

--- src/SESport.Core/GlobalUsings.cs
+++ src/SESport.Core/GlobalUsings.cs
@@ -1,5 +1,3 @@
 global using SESport.Core.Domain;
 global using SESport.Core.Identifiers;
-global using SESport.Core.Ingestion;
-global using SESport.Core.Queries;
 global using SESport.Core.Sources;
