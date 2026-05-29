3eef53a69f40208ae95a62423540bb005bc9aa10
Daniel Solin <daniel@solin.org>
2026-05-29 15:47:11 +0200

    Move migration PowerShell helper up one level

---
+++ database/run-migrations.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = "Stop"
+
+$database = $env:SESPORT_POSTGRES_DB
+$user = $env:SESPORT_POSTGRES_USER
+
+if ([string]::IsNullOrWhiteSpace($database))
+{
+   $database = "sesport"
+}
+
+if ([string]::IsNullOrWhiteSpace($user))
+{
+   $user = "sesport"
+}
+
+$migrations = Get-ChildItem `
+   -Path "database/migrations" `
+   -Filter "*.sql" |
+   Sort-Object Name
+
+foreach ($migration in $migrations)
+{
+   $containerPath = "/migrations/$($migration.Name)"
+
+   Write-Host "Running $($migration.Name)"
+
+   docker compose exec -T postgres `
+      psql `
+      -U $user `
+      -d $database `
+      -v ON_ERROR_STOP=1 `
+      -f $containerPath
+}
