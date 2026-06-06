55c8a1a97bfdc98e7c7b0fde332d253c24b97639
Daniel Solin <daniel@solin.org>
2026-06-06 11:16:42 +0200

    Implement today/tomorrow on main page.

--- src/SESport.Web/Pages/Index.cshtml.cs
+++ src/SESport.Web/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SESport.Web.Data;
 
@@ -5,20 +6,49 @@ namespace SESport.Web.Pages;
 
 public class IndexModel(ActivityRepository repository) : PageModel
 {
+   public const string TodayDay = "Today";
+   public const string TomorrowDay = "Tomorrow";
+
    public IReadOnlyList<ActivityListItem> Activities { get; private set; } =
       [];
+
+   [BindProperty(SupportsGet = true, Name = "day")]
+   public string? Day { get; set; } = TodayDay;
 
    public string? LoadError { get; private set; }
 
    public async Task OnGetAsync(CancellationToken cancellationToken)
    {
+      Day = NormalizeDay(Day) ?? TodayDay;
+
       try
       {
-         Activities = await repository.GetTodaysAsync(cancellationToken);
+         Activities = Day switch
+         {
+            TodayDay => await repository.GetTodaysAsync(
+               cancellationToken
+            ),
+            TomorrowDay => await repository.GetTomorrowsAsync(
+               cancellationToken
+            ),
+            _ => await repository.GetTodaysAsync(cancellationToken)
+         };
       }
       catch (Exception exception)
       {
          LoadError = exception.Message;
       }
+   }
+
+   private static string? NormalizeDay(string? day)
+   {
+      return day switch
+      {
+         TodayDay => TodayDay,
+         TomorrowDay => TomorrowDay,
+         "" => TodayDay,
+         null => TodayDay,
+         _ => null
+      };
    }
 }
