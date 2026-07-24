95537fcbafc46be92971129d52672e30b1f71310
Daniel Solin <daniel@solin.org>
2026-07-24 05:45:22 +0200

    Reload base url on visibilitychange

--- src/SESport.Web/wwwroot/js/site.js
+++ src/SESport.Web/wwwroot/js/site.js
@@ -2,7 +2,9 @@
    document.addEventListener("visibilitychange", () => {
       if(!document.hidden)
       {
-         window.location.reload();
+         const baseUrl =
+            `${window.location.origin}${window.location.pathname}`;
+         window.location.assign(baseUrl);
       }
    });
 
