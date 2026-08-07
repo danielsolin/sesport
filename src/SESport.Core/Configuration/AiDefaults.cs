a189be08731e63d723d591c2b8a5a3d548558047
Daniel Solin <daniel@solin.org>
2026-08-07 10:53:30 +0200

    Remove unused AI configuration defaults

--- src/SESport.Core/Configuration/AiDefaults.cs
+++ src/SESport.Core/Configuration/AiDefaults.cs
@@ -3,9 +3,6 @@ namespace SESport.Core.Configuration;
 public static class AiDefaults
 {
    public const int DefaultMaxOutputTokens = 8192;
-   public const int MalformedResponseAttempts = 3;
-   public const double ActivitySearchTemperature = 0.1;
-   public const string GoogleApiKeyHeader = "x-goog-api-key";
 
    public const string OpenRouterMetadataHeader =
       "X-OpenRouter-Experimental-Metadata";
@@ -20,8 +17,6 @@ public static class AiDefaults
       TimeSpan.FromMinutes(20);
    public static readonly TimeSpan SearxngHttpClientTimeout =
       TimeSpan.FromSeconds(60);
-   public static readonly TimeSpan GoogleWebSearchHttpClientTimeout =
-      TimeSpan.FromSeconds(30);
    public static readonly TimeSpan WebPageContentHttpClientTimeout =
       TimeSpan.FromSeconds(30);
 
